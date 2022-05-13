513b08de86bded6da4d7951daf74e7b87f1ee776
Shivani <shivani.anand@ens.enterprises>
2022-05-13 17:23:16 +0530

    redirect change commit

--- src/Pages/Shopify.js
+++ src/Pages/Shopify.js
@@ -4,12 +4,13 @@ import { useEffect } from "react";
 const Shopify = () => {
 
   let scopes = 'read_orders,write_orders';
-  let apiKey = 'b30f10c71e61650acf31a21c34420549';
-  // let apiSecretKey = 'b8426d9b6134be55c49cc055ecaad5b3';
+  let apiKey = process.env.React_App_SHOPIFY_API_KEY;
+  // let apiSecretKey = 'process.env.React_App_SHOPIFY_API_SECRET_KEY';
   let redirect_uri = 'https://tabicon.netlify.app/shopify/callback'
   let oAuthUrl = `https://blue-heavenn.myshopify.com/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&redirect_uri=${redirect_uri}&state=sasassa321dsad`;
 
-  useEffect(() => {
+  useEffect((e) => {
+    e.preventDefault();
     window.location.href = oAuthUrl;
 
   }, )
